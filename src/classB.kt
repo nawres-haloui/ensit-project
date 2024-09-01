c15992e1a9ac96ffafbc4e76a41d1d1fea78635a
nawrashaloui <nawras.haloui@maibornwolff.de>
2024-09-01 16:26:27 +0100

    remove changes

--- src/classB.kt
+++ src/classB.kt
@@ -1,14 +1,5 @@
 class classB {
     val number = 2
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as classB
-
-        return number == other.number
-    }
-
     override fun hashCode(): Int {
         return number
     }
