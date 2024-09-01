1176acdeb678697cfe431320855b9463de2a503d
nawrashaloui <nawras.haloui@maibornwolff.de>
2024-09-01 16:22:36 +0100

    add new classA

--- src/Main.kt
+++ src/Main.kt
@@ -7,8 +7,7 @@ fun main() {
     println("Hello, " + name + "!")
 
     for (i in 1..5) {
-c        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+
         println("i = $i")
     }
 }
--- src/classA.kt
+++ src/classA.kt
@@ -1,2 +1,16 @@
 class classA {
+    val number = 2
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as classA
+
+        return number == other.number
+    }
+
+    override fun hashCode(): Int {
+        return number
+    }
+
 }
